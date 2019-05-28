dd884bc37ea902c9bce8ae6b54d58ce0032e01fa
talisonmaturana <talisondaniel@github.com>
2019-05-28 19:17:31 -0300

    Fix user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
-class User {
-    constructor(id, name, email, password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-}
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-module.exports = { User };
+var user = Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    email: String,
+    password: String
+}, { collection: 'users' });
+
+module.exports = mongoose.model('User', user);
